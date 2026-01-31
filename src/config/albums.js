46490ea35d24f49b335a3a0c7a663abd3ef8bb3d
mathiore <matheus.costa.raimundo@gmail.com>
2026-01-31 19:31:43 -0300

    reorganization of album section new config files

---
+++ src/config/albums.js
@@ -0,0 +1,111 @@
+export const albums = [
+  { 
+    id: 'lambo', 
+    title: 'Lamborghini Revuelto', 
+    modelPath: '/models/lambo.glb',
+    cover: '/models/Lambo/IMG_0043.jpg', 
+    images: [
+      '/models/Lambo/IMG_0043.jpg',
+      '/models/Lambo/IMG_0054.jpg',
+      '/models/Lambo/IMG_0068.jpg',
+      '/models/Lambo/IMG_0085.jpg',
+      '/models/Lambo/IMG_0120.jpg',
+      '/models/Lambo/IMG_0135.jpg',
+      '/models/Lambo/IMG_0157.jpg',
+      '/models/Lambo/IMG_0161.jpg'
+    ]
+  },
+  { 
+    id: 'gallardo', 
+    title: 'Lamborghini Gallardo', 
+    modelPath: '/models/gallardo.glb',
+    cover: '/models/Gallardo/IMG_6760.jpg', 
+    images: [
+      '/models/Gallardo/IMG_6760.jpg',
+      '/models/Gallardo/Sem-Título-1_01.jpg',
+      '/models/Gallardo/Sem-Título-1_02.jpg',
+      '/models/Gallardo/Sem-Título-1_03.jpg',
+      '/models/Gallardo/Sem-Título-1_04.jpg',
+      '/models/Gallardo/Sem-Título-1_05.jpg',
+      '/models/Gallardo/Sem-Título-1_06.jpg'
+    ]
+  },
+  { 
+    id: 'auditt', 
+    title: 'Audi TT', 
+    modelPath: '/models/auditt.glb',
+    cover: '/models/Audi TT/IMG_8885.jpg', 
+    images: [
+      '/models/Audi TT/IMG_8885.jpg',
+      '/models/Audi TT/IMG_8938.jpg',
+      '/models/Audi TT/IMG_8947.jpg',
+      '/models/Audi TT/adui11_01.jpg',
+      '/models/Audi TT/adui11_02.jpg',
+      '/models/Audi TT/adui11_03.jpg',
+      '/models/Audi TT/adui11_04.jpg',
+      '/models/Audi TT/adui11_05.jpg',
+      '/models/Audi TT/adui11_06.jpg'
+    ]
+  },
+  { 
+    id: 'mclaren', 
+    title: 'McLaren', 
+    modelPath: '/models/mclaren.glb',
+    cover: '/models/Mc laren/IMG_6788.jpg', 
+    images: [
+      '/models/Mc laren/IMG_6779.jpg',
+      '/models/Mc laren/IMG_6788.jpg',
+      '/models/Mc laren/IMG_6799.jpg',
+      '/models/Mc laren/IMG_6818.jpg',
+      '/models/Mc laren/post-6x6_01.jpg',
+      '/models/Mc laren/post-6x6_02.jpg'
+    ]
+  },
+  { 
+    id: 'masserati', 
+    title: 'Maserati', 
+    modelPath: '/models/masserati.glb',
+    cover: '/models/Masserati/IMG_6825.png', 
+    images: [
+      '/models/Masserati/IMG_6825.png',
+      '/models/Masserati/MASERATII_01.png',
+      '/models/Masserati/MASERATII_02.png',
+      '/models/Masserati/MASERATII_03.png'
+    ]
+  },
+  { 
+    id: 'mustang', 
+    title: 'Mustang Blue', 
+    cover: '/models/Mustang blue/DSC01243.jpg', 
+    images: [
+      '/models/Mustang blue/DSC01238.jpg',
+      '/models/Mustang blue/DSC01243-2.jpg',
+      '/models/Mustang blue/DSC01243.jpg'
+    ]
+  },
+  { 
+    id: 'jetta', 
+    title: 'Jetta', 
+    cover: '/models/Jetta/IMG_0211.jpg', 
+    images: [
+      '/models/Jetta/IMG_0211.jpg',
+      '/models/Jetta/IMG_0254.jpg',
+      '/models/Jetta/IMG_0571.jpg',
+      '/models/Jetta/IMG_0580.jpg',
+      '/models/Jetta/IMG_0583.jpg',
+      '/models/Jetta/IMG_0598.jpg'
+    ]
+  },
+  { 
+    id: 'opala', 
+    title: 'Opala', 
+    cover: '/models/Opala/IMG_0382.jpg', 
+    images: [
+      '/models/Opala/IMG_0382.jpg',
+      '/models/Opala/IMG_0459.jpg',
+      '/models/Opala/IMG_0501.jpg',
+      '/models/Opala/IMG_0515.jpg',
+      '/models/Opala/IMG_0663.jpg'
+    ]
+  }
+];
