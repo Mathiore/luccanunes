aac33b996f433776578201a64b7917f838f5e06a
mathiore <matheus.costa.raimundo@gmail.com>
2026-02-10 20:41:11 -0300

    removed old stuff

--- src/config/cars.js
+++
@@ -1,8 +0,0 @@
-export const cars = [
-    { path: '/models/auditt.glb', scale: 0.8, name: 'Audi TT', albumId: 'auditt' },
-    { path: '/models/ferrari.glb', scale: 0.8, name: 'Ferrari', albumId: 'ferrari' },
-    { path: '/models/gallardo.glb', scale: 0.8, name: 'Gallardo', albumId: 'gallardo' },
-    { path: '/models/lambo.glb', scale: 0.8, name: 'Revuelto', albumId: 'lambo' },
-    { path: '/models/masserati.glb', scale: 0.8, name: 'Maserati', albumId: 'masserati' },
-    { path: '/models/mclaren.glb', scale: 0.8, name: 'McLaren', albumId: 'mclaren' },
-];
