eeccc587bcb07921835327e6a299ac96eeafcb93
Matheus Raimundo <61631379+Mathiore@users.noreply.github.com>
2026-02-13 20:18:25 -0300

    Change base path from '/luccaportfolio/' to '/luccanunes/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/luccaportfolio/',
+  base: '/luccanunes/',
   plugins: [vue()],
   build: {
     chunkSizeWarningLimit: 1000, // Aumenta o limite para 1000kB
